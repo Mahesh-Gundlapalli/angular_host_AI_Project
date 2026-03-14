d0b466ef09f3cb5739a389baafb89ba80699e7b9
Mahesh-Gundlapalli <157827335+Mahesh-Gundlapalli@users.noreply.github.com>
2026-03-14 17:14:36 +0530

    Update production API base URL and add second URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:8080/api/v1'
+  apiBaseUrl: 'https://spring-ai-docker-host.onrender.com/api/v1',
+  apiBaseUrl2: 'http://localhost:8080/api/v1'
 };
